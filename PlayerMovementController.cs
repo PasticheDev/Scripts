184ae9dd8a25c8cfc5dda273ccf8320bdb4e7c11
PasticheDev <41286061+PasticheDev@users.noreply.github.com>
2018-07-16 11:02:43 +0200

    Add files via upload
    Movement Script 2D including animations

---
+++ PlayerMovementController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+
+    public float speed = 10;
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector2 dir = new Vector2(h, v);
+        GetComponent<Rigidbody2D>().velocity = dir.normalized * speed;
+        
+        GetComponent<Animator>().SetBool("isFlyingUp", (v > 0));
+        GetComponent<Animator>().SetBool("isFlyingLeft", (h < 0));
+        GetComponent<Animator>().SetBool("isFlyingRight", (h > 0));
+
+    }
+
+}
